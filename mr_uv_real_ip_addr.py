60648a32c36e5fce87b7c5d68f5fc89f483ca7aa
ChenHao <275258836@qq.com>
2016-09-24 11:04:04 +0800

    解决显示中文问题

--- mr_uv_real_ip_addr.py
+++ mr_uv_real_ip_addr.py
@@ -3,16 +3,22 @@
 
 from mrjob.job import MRJob
 from mrjob.step import MRStep
+from mrjob.protocol import RawProtocol
 from ng_line_parser import NgLineParser
 
 import pandas as pd
 import heapq
 import socket
 import struct
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 
 class MRUVRrealIpAddr(MRJob):
 
+    OUTPUT_PROTOCOL = RawProtocol
     ng_line_parser = NgLineParser()
 
     def mapper(self, _, line):
@@ -21,18 +27,18 @@ class MRUVRrealIpAddr(MRJob):
 
     def reducer_sum(self, key, values):
         """统计 VU"""
-        yield None, [sum(values), key]
+        yield None, [str(sum(values)), key]
 
     def init_ip_addr_df(self):
         """读取IP Addr 文件构造 DataFrame 文件"""
         cols = ['id', 'ip_start_num', 'ip_end_num',
                 'ip_start', 'ip_end', 'addr', 'operator']
-        area_ip_path = '/tmp/nginx_log_parse/area_ip.csv'     
+        area_ip_path = '/root/script/nginx_log_parse/area_ip.csv'     
         self.ip_addr_df = pd.read_csv(area_ip_path, sep='\t', names=cols, index_col='id')
 
     def reducer_top100(self, _, values):
         """访问数降序"""
-        for cnt, ip in heapq.nlargest(100, values):
+        for cnt, ip in heapq.nlargest(100, values, key=lambda x: int(x[0])):
             ip_num = -1
             try:
                 # 将IP转化成INT/LONG 数字
@@ -42,11 +48,9 @@ class MRUVRrealIpAddr(MRJob):
                                           (ip_num <= self.ip_addr_df.ip_end_num)]
                 # 通过索引值获得获得 地址
                 addr = addr_df.at[addr_df.index.tolist()[0], 'addr']
-                print cnt, '{ip}    {addr}'.format(ip=ip, addr=addr)
+                yield cnt, '{ip}    {addr}'.format(ip=ip, addr=addr)
             except:
-                print cnt, ip 
-
-
+                yield cnt, ip 
 
     def steps(self):
         return [
